2a10b37d139f17f9bccb42f619eca7f1b6a3f71d
Jaison Rodrigo Dallabrida <jaison.dallabrida@FLOWTI.COM.BR>
2021-07-29 21:44:59 -0300

    Autoload do hcodebr classes.

--- vendor/composer/InstalledVersions.php
+++ vendor/composer/InstalledVersions.php
@@ -25,24 +25,24 @@ class InstalledVersions
 private static $installed = array (
   'root' => 
   array (
-    'pretty_version' => '1.0.0+no-version-set',
-    'version' => '1.0.0.0',
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
     'aliases' => 
     array (
     ),
-    'reference' => NULL,
+    'reference' => '28ebeafb98b6f87be817ba2fbafa87d7ce34a39e',
     'name' => '__root__',
   ),
   'versions' => 
   array (
     '__root__' => 
     array (
-      'pretty_version' => '1.0.0+no-version-set',
-      'version' => '1.0.0.0',
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
       'aliases' => 
       array (
       ),
-      'reference' => NULL,
+      'reference' => '28ebeafb98b6f87be817ba2fbafa87d7ce34a39e',
     ),
     'phpmailer/phpmailer' => 
     array (
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit1ec06d6d37ee4a4109895904117be6ee
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'H' => 
+        array (
+            'Hcode\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Hcode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -42,7 +52,8 @@ class ComposerStaticInit1ec06d6d37ee4a4109895904117be6ee
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInit1ec06d6d37ee4a4109895904117be6ee::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit1ec06d6d37ee4a4109895904117be6ee::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit1ec06d6d37ee4a4109895904117be6ee::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit1ec06d6d37ee4a4109895904117be6ee::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit1ec06d6d37ee4a4109895904117be6ee::$classMap;
 
