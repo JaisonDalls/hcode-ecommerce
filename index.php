bc17a2d51e9387411d4e1a215e986738d2deaf23
Jaison Rodrigo Dallabrida <jaisondalls@gmail.com>
2021-08-14 23:18:27 -0300

    Atualização do projeto

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+
+require_once("vendor/autoload.php");
+
+use \Slim\Slim;
+use \Hcode\DB\Sql;
+
+$app = new Slim();
+
+$app->config('debug', true);
+
+$app->get('/', function() {
+    
+	$sql = new Sql();
+	$results = $sql->select("SELECT * FROM db_users");
+	
+	echo json_encode($results);
+
+});
+
+$app->run();
+
+ ?>
